2984569b75df21fcd98a0ea3f70ac9a386788821
Neil <h66r10@gmail.com>
2021-05-11 05:44:05 +0200

    working version

--- src/main/java/PigSpawnerFinder/PigSpawnerFromWorldSeed.java
+++ src/main/java/PigSpawnerFinder/PigSpawnerFromWorldSeed.java
@@ -11,32 +11,61 @@ import kaptainwutax.mcutils.version.MCVersion;
 import kaptainwutax.seedutils.lcg.LCG;
 import kaptainwutax.terrainutils.terrain.OverworldChunkGenerator;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 import static PigSpawnerFinder.PigSpawnerFinder.processCarverSeed;
+
 
 public class PigSpawnerFromWorldSeed {
 	// this is non optimized
 	public static void main(String[] args) {
-		long worldSeed = -2464334185163669257L;
 
-		int size = 2000;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter worldseed : ");
+		String ws = scanner.nextLine();
+		long worldSeed;
+		try {
+			worldSeed = Long.parseLong(ws);
+		} catch (NumberFormatException e) {
+			worldSeed = ws.hashCode();
+			System.err.println("You inputted a wrong world seed, we converted it to a string one " + worldSeed);
+		}
+		System.out.println("Using worldseed : " + worldSeed);
+		System.out.println("Enter half size to search for (in chunks) : ");
+		String sz = scanner.nextLine();
+		int size;
+		try {
+			size = Integer.parseInt(sz);
+		} catch (NumberFormatException e) {
+			System.out.println("Sorry you inputed a wrong size (too large or something)");
+			e.printStackTrace();
+			return;
+		}
+		System.out.printf("Searching an area of %dx%d chunks\n",size*2,size*2);
 		for (int chunkX = -size; chunkX < size; chunkX++) {
+			int finalChunkX = chunkX;
+			long finalWorldSeed = worldSeed;
+//			IntStream.range(-size, size).parallel().forEach(
+//					chunkZ -> processForChunk(finalWorldSeed, finalChunkX, chunkZ)
+//			);
+
 			for (int chunkZ = -size; chunkZ < size; chunkZ++) {
-				processForChunk(worldSeed,chunkX,chunkZ);
+				processForChunk(-2464334185163669257L, finalChunkX, chunkZ);
 			}
 		}
-
+		System.out.println("We are done, if you didn't see any STEP X (from 1 to 3, 3 being the final true one)\n" +
+				"Then you have a non possible worldseed or a too small of an area, remember those are rare.");
 	}
 
 
-	public static void processForChunk(long worldSeed,int chunkX,int chunkZ){
+	public static void processForChunk(long worldSeed, int chunkX, int chunkZ) {
+		System.out.println(chunkX+ " "+chunkZ);
 		ArrayList<Spawner> spawners = new ArrayList<>();
+		// get the spawner from the mineshaft
 		ArrayList<StructurePiece> pieces = MineshaftGenerator.generateForChunk(
 				WorldSeed.toStructureSeed(worldSeed),
 				chunkX, chunkZ, true, spawners
@@ -47,39 +76,41 @@ public class PigSpawnerFromWorldSeed {
 			if (spawner.y < 58 || spawner.y > 59) continue;
 
 			//Check if it can be at 9 9 but not near supports
-			int offsetChunkX = spawner.x >> 4;
-			int offsetChunkZ = spawner.z >> 4;
-			int spawnerOffsetX = spawner.x - (offsetChunkX << 4);
-			int spawnerOffsetZ = spawner.z - (offsetChunkZ << 4);
-			if (!(spawner.direction.axis == Direction.Axis.X && spawnerOffsetZ == 9 && (spawnerOffsetX == 8 || spawnerOffsetX == 10)) && !(spawner.direction.axis == Direction.Axis.Z && spawnerOffsetX == 9 && (spawnerOffsetZ == 8 || spawnerOffsetZ == 10)))
-				continue;
-			//Check if it isn't too close to mesa
-			if (Math.abs(offsetChunkX) < 2 && Math.abs(offsetChunkZ) < 2) continue;
+			int spawnerChunkX = spawner.x >> 4;
+			int spawnerChunkZ = spawner.z >> 4;
+			int spawnerOffsetX = spawner.x & 15;
+			int spawnerOffsetZ = spawner.z & 15;
+			if (spawner.direction.axis == Direction.Axis.X && spawnerOffsetZ == 9 && (spawnerOffsetX == 8 || spawnerOffsetX == 10) ||
+					spawner.direction.axis == Direction.Axis.Z && spawnerOffsetX == 9 && (spawnerOffsetZ == 8 || spawnerOffsetZ == 10)) {
 
-			//Check if there are no other corridors in the same chunk generated before the one with the spawner (meaning no random calls before our corridor)
-			int piecesBeforeSpawner = 0;
-			BlockBox spawnerBox = new BlockBox(spawner.x, spawner.y, spawner.z, spawner.x, spawner.y, spawner.z);
-			BlockBox chunk = new BlockBox(offsetChunkX << 4, 0, offsetChunkZ << 4, (offsetChunkX << 4) + 15, 255, (offsetChunkZ << 4) + 15);
-			for (StructurePiece piece : pieces) {
-				if (piece.boundingBox.intersects(chunk)) {
-					if (piece.boundingBox.intersects(spawnerBox)) break;
-					else if (piece instanceof MineshaftGenerator.MineshaftCorridor) {
-						piecesBeforeSpawner = 1;
-						break;
+				//Check if it isn't too close to mesa
+				if (Math.abs(spawnerChunkX) < 2 && Math.abs(spawnerChunkZ) < 2) continue;
+
+				//Check if there are no other corridors in the same chunk generated before the one with the spawner (meaning no random calls before our corridor)
+				int piecesBeforeSpawner = 0;
+				BlockBox spawnerBox = new BlockBox(spawner.x, spawner.y, spawner.z, spawner.x, spawner.y, spawner.z);
+				BlockBox chunk = new BlockBox(spawnerChunkX << 4, 0, spawnerChunkZ << 4, (spawnerChunkX << 4) + 15, 255, (spawnerChunkZ << 4) + 15);
+				for (StructurePiece piece : pieces) {
+					if (piece.boundingBox.intersects(chunk)) {
+						if (piece.boundingBox.intersects(spawnerBox)) break;
+						else if (piece instanceof MineshaftGenerator.MineshaftCorridor) {
+							piecesBeforeSpawner = 1;
+							break;
+						}
 					}
 				}
-			}
-			if (piecesBeforeSpawner != 0) continue;
+				if (piecesBeforeSpawner != 0) continue;
 
-			System.out.println("Spawner could work for : " + spawner);
-			processCarverSeed(worldSeed, spawner.x, spawner.y, spawner.z, spawner.direction, spawner.length);
+				System.out.println("STEP 1: Spawner could work for : " + spawner);
+				findOutIfCorrect(worldSeed, spawner);
+			}
 		}
 	}
 
 	public static void findOutIfCorrect(long worldseed, Spawner spawner) {
 		ChunkRand rand = new ChunkRand();
 		// everything here can be handled with the structure seed only (the lower 48 bits)
-		long structureSeed= WorldSeed.toStructureSeed(worldseed);
+		long structureSeed = WorldSeed.toStructureSeed(worldseed);
 
 		int m = spawner.length * 5;
 		LCG skipCeiling = LCG.JAVA.combine(m * 3L);
@@ -92,14 +123,15 @@ public class PigSpawnerFromWorldSeed {
 		int spawnerChunkX = spawner.x >> 4;
 		int spawnerChunkZ = spawner.z >> 4;
 
-		int spawnerOffsetX = spawner.x & 15 ;
-		int spawnerOffsetZ =spawner.x & 15;
+		int spawnerOffsetX = spawner.x & 15;
+		int spawnerOffsetZ = spawner.z & 15;
 		int spawnerOffset = spawner.direction.axis == Direction.Axis.X ? spawnerOffsetX : spawnerOffsetZ;
 
 
 		//Check for buried treasure
 		rand.setRegionSeed(structureSeed, spawnerChunkX, spawnerChunkZ, 10387320, MCVersion.v1_16);
 		if (rand.nextFloat() >= 0.01F) return;
+
 
 		//Check for cobwebs and spawner position
 		//The spawner piece is the first corridor piece generated in this chunk so there are no random calls before it
@@ -124,8 +156,7 @@ public class PigSpawnerFromWorldSeed {
 		}
 
 		int spawnerShift = rand.nextInt(3) - 1;
-		int spawnerShiftReal = spawner.direction == Direction.NORTH ||  spawner.direction == Direction.WEST ? -spawnerShift : spawnerShift;
-//                    System.out.println(spawnerOffset + spawnerShift);
+		int spawnerShiftReal = spawner.direction == Direction.NORTH || spawner.direction == Direction.WEST ? -spawnerShift : spawnerShift;
 		if (spawnerOffset + spawnerShiftReal != 9) return;
 
 		//Check for no cobwebs near the spawner
@@ -155,8 +186,8 @@ public class PigSpawnerFromWorldSeed {
 		}
 		if (hasCobwebsNearby) return;
 
-		BPos spawnerPos=new BPos((spawnerChunkX << 4) + 9,spawner.y,(spawnerChunkZ << 4) + 9);
-		System.out.println("Structure: " + structureSeed + " Center: " + spawnerChunkX + " " + spawnerChunkX + " Spawner: " +spawnerPos);
+		BPos spawnerPos = new BPos((spawnerChunkX << 4) + 9, spawner.y, (spawnerChunkZ << 4) + 9);
+		System.out.println("STEP 2: Spawner passed the buried treasure test : " + spawner);
 		processWorldSeed(worldseed, spawnerPos);
 
 	}
@@ -171,10 +202,10 @@ public class PigSpawnerFromWorldSeed {
 	}};
 
 
-	public static void  processWorldSeed(long worldSeed, BPos spawnerPos){
+	public static void processWorldSeed(long worldSeed, BPos spawnerPos) {
 		OverworldBiomeSource biomeSource;
 		OverworldChunkGenerator chunkGenerator;
-		CPos spawnerChunkPos=spawnerPos.toChunkPos();
+		CPos spawnerChunkPos = spawnerPos.toChunkPos();
 		//Check biomes
 		biomeSource = new OverworldBiomeSource(MCVersion.v1_16, worldSeed);
 		// those two checks are super intensive, that's why we do it at last
@@ -183,18 +214,10 @@ public class PigSpawnerFromWorldSeed {
 //            System.out.println("Good biomes: " + worldSeed);
 
 		//Check depth above spawner
-		PrintStream old = System.out;
-		System.setOut(new PrintStream(new OutputStream() {
-			@Override
-			public void write(int b) throws IOException {}
-		}));
 		chunkGenerator = new OverworldChunkGenerator(biomeSource);
-		System.setOut(old);
 		int height = chunkGenerator.getHeightInGround(spawnerPos.getX(), spawnerPos.getZ());
 		int depth = height - spawnerPos.getY();
-//            System.out.println(depth);
 		if (depth < 3 || depth > 6) return;
-//            System.out.println("Good central height: " + worldSeed);
 
 		//Check depth nearby to avoid water
 		boolean good = true;
@@ -210,6 +233,7 @@ public class PigSpawnerFromWorldSeed {
 		}
 		if (!good) return;
 
-		System.out.println("Good nearby height: " + worldSeed + " " + spawnerPos);
+		System.out.printf("STEP 3 (FINAL) : Found spawner : /tp @p %d %d %d for worldseed %d\n",
+				spawnerPos.getX(), spawnerPos.getY(), spawnerPos.getZ(), worldSeed);
 	}
 }
